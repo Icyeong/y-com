3e5793511f2d49df2587a1a445955d16ee76fc12
chaeyeong Imm <cyim@chonf.co.kr>
2024-02-07 16:44:51 +0900

    msw 세팅

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,26 @@
+import { http, HttpResponse } from "msw";
+
+export const handlers = [
+  http.post("/api/login", () => {
+    return HttpResponse.json(
+      {
+        userId: 1,
+        nickname: "icy",
+        id: "icyeong",
+        image: "/5Udwvqim.jpg",
+      },
+      {
+        headers: {
+          "Set-Cookie": "connect.sid=msw-cookie;HttpOnly;Path=/",
+        },
+      }
+    );
+  }),
+  http.post("/api/logout", () => {
+    return HttpResponse.json(null, {
+      headers: {
+        "Set-Cookie": "connect.sid=;HttpOnly;Path=/;Max-Age=0",
+      },
+    });
+  }),
+];
